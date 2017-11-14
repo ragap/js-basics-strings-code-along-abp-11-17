fa3d0cf5346fca2d39ebfab6a951fd3f0971e8ac
ragas <sudha.puttagunta@gmail.com>
2017-11-14 11:31:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = 'Grace Hopper';
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-const shortGreeting = `Welcome,${currentUser} ! `;
+const shortGreeting = `Welcome, ${currentUser} ! `;
